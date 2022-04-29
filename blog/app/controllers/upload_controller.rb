5bd9f0f30392020739f397c91bae1eb3a16b9eb8
Naing Lynn Aung <unrivaledking1@gmail.com>
2022-04-29 09:45:35 +0630

    Updating Tuto08 and Tuto09

--- blog/app/controllers/upload_controller.rb
+++ blog/app/controllers/upload_controller.rb
@@ -5,22 +5,22 @@ class UploadController < ApplicationController
     @folder_name = params[:folder]
 
     if @folder_name == nil
-        redirect_to upload_path, notice: "File can't be empty"
+        redirect_to tuto09_upload_path, notice: "File can't be empty"
     else
 
       @img_name = params[:image].original_filename
 
-      if(Dir.exists?("./app/views/#{@folder_name}"))
-        redirect_to upload_path, notice: "This Folder Name is Existing"
+      if(Dir.exists?("./app/views/upload/#{@folder_name}"))
+        redirect_to tuto09_upload_path, notice: "This Folder Name is Existing"
 
       else
 
         session[:folder_name] = @folder_name
         session[:img_name] = @img_name
 
-        Dir.mkdir("./app/views/#{@folder_name}")
+        Dir.mkdir("./app/views/upload/#{@folder_name}")
 
-        File.open("./app/views/#{@folder_name}/#{@img_name}", "w") do |f|
+        File.open("./app/views/upload/#{@folder_name}/#{@img_name}", "w") do |f|
           f.write params[:image]
 
         end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'home', to: 'home#index', as: 'home'
+  root 'home#index'
+  get '/tuto08/home', to: 'home#index'
+  
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  get 'upload', to: 'upload#index', as: 'upload'
+  get '/tuto09/upload', to: 'upload#index'
   resources :upload
-  post "/create_folder", to: "upload#create_folder"
-  get "/create_folder", to: "upload#show"
+  post "/create_folder", to: 'upload#create_folder'
+  get "/create_folder", to: 'upload#show'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
